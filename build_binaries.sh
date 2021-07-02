49608fc5fb2e17eda24929139916ac6321bfd37d
Kamil Rakoczy <krakoczy@antmicro.com>
2021-07-02 14:24:17 +0200

    surelog: build with independent code on
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -2,7 +2,11 @@
 set -ex
 INSTALL_PATH=$PWD/image
 #Surelog
-cd Surelog && make PREFIX=$INSTALL_PATH release install -j $(nproc) && cd ..
+cd Surelog && \
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_POSITION_INDEPENDENT_CODE=ON -S . -B build && \
+	cmake --build build -j $(nproc) && \
+	cmake --install build && \
+	cd ..
 #Yosys
 cd yosys && make PREFIX=$INSTALL_PATH install -j $(nproc) && cd ..
 #sv2v
