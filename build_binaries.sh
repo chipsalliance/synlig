aa8af1d403118142ce8ed3fb973ca22d2e621b08
Kamil Rakoczy <krakoczy@antmicro.com>
2021-09-24 13:03:41 +0200

    CI: use self-hosted runners
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -2,13 +2,15 @@
 set -ex
 INSTALL_PATH=$PWD/image
 #Surelog
-cd Surelog && \
-	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_POSITION_INDEPENDENT_CODE=ON -S . -B build && \
-	cmake --build build -j $(nproc) && \
-	cmake --install build && \
-	cd ..
+cd Surelog
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_POSITION_INDEPENDENT_CODE=ON -S . -B build
+cmake --build build -j $(nproc)
+cmake --install build
+cd ..
 #Yosys
-cd yosys && make PREFIX=$INSTALL_PATH install -j $(nproc) && cd ..
+cd yosys
+make CONFIG=gcc PREFIX=$INSTALL_PATH install -j $(nproc)
+cd ..
 #UHDM plugin
 export PATH=$INSTALL_PATH/bin:${PATH}
 UHDM_INSTALL_DIR=$INSTALL_PATH make -C $PWD/yosys-symbiflow-plugins/ install -j$(nproc)
