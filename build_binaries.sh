c19cbcedbf5e86871f69e3688de5b22917230507
Kamil Rakoczy <krakoczy@antmicro.com>
2021-12-16 13:28:25 +0100

    Add option to build sv2v (default skip)
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -5,6 +5,29 @@ if [ -z "$INSTALL_PATH" ]; then
     INSTALL_PATH=$PWD/image
 fi
 
+SKIP_YOSYS=0
+BUILD_SV2V=0
+
+for arg in $@; do
+  echo $arg
+  case "$arg" in
+    -y|--skip-yosys)
+      SKIP_YOSYS=1
+      ;;
+    -s|--build-sv2v)
+      BUILD_SV2V=1
+      ;;
+    *)
+      if [ ${CASTOR} -eq 1 ]; then
+        CASTOR_PATH=${arg}
+      else
+        echo "Usage: build_binaries [-y|--skip-yosys] [-s|--build-sv2v]"
+        exit 1
+      fi
+      ;;
+  esac
+done
+
 #Surelog
 cd Surelog
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_POSITION_INDEPENDENT_CODE=ON -S . -B build
@@ -12,7 +35,7 @@ cmake --build build -j $(nproc)
 cmake --install build
 cd ..
 #Yosys
-if [ "$1" != "--skip-yosys" ]; then
+if [ "$SKIP_YOSYS" -eq "0" ]; then
 cd yosys
 make CONFIG=gcc PREFIX=$INSTALL_PATH install -j $(nproc)
 cd ..
@@ -21,5 +44,7 @@ fi
 export PATH=$INSTALL_PATH/bin:${PATH}
 UHDM_INSTALL_DIR=$INSTALL_PATH make -C $PWD/yosys-symbiflow-plugins/ install -j$(nproc)
 #sv2v
+if [ "$BUILD_SV2V" -eq "1" ]; then
 wget -qO- https://get.haskellstack.org/ | sh -s - -f -d $INSTALL_PATH/bin
 make -C $PWD/sv2v && cp $PWD/sv2v/bin/sv2v $INSTALL_PATH/bin
+fi
