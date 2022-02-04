288cb5760be0b32fd0d76246409632f1c4b7ad14
Krzysztof Bieganski <kbieganski@antmicro.com>
2022-02-04 13:12:47 +0100

    Add plugin-only releases (#399)
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

---
+++ install_plugin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if ! command -v yosys-config; then
+    echo "yosys-config not found! Do you have Yosys installed on your system?"
+    exit 1
+fi
+
+YOSYS_DATDIR=$(yosys-config --datdir)
+YOSYS_PLUGIN_DIR=$YOSYS_DATDIR/plugins
+
+INSTALL_SCRIPT_DIR=$(dirname $(realpath $0))
+UHDM_PLUGIN_PATH=$INSTALL_SCRIPT_DIR/image/share/yosys/plugins/uhdm.so
+
+mkdir -p $YOSYS_PLUGIN_DIR
+cp -v $UHDM_PLUGIN_PATH $YOSYS_PLUGIN_DIR
