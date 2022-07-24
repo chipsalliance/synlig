18e0ad7b3d440fe9042c50ba97790741d2f7f110
Alain Dargelas <alain.dargelas@gmail.com>
2022-07-23 22:19:39 -0700

    1600 Formal tests

--- run_formal_verif.sh
+++ run_formal_verif.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 # Copyright 2022 Alain Dargelas
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +15,13 @@ set -e
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Usage:
+# ./build_binaries.sh --build-sv2v
+# ./run_formal_verif.sh
+
+
+
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -38,8 +44,8 @@ cd ../../
 cd build
 
 echo "Yosys Regression"
-../Surelog/tests/regression.tcl search_dir=../yosys/tests path=$SCRIPTPATH/Surelog/build/bin/ verification
+../Surelog/tests/regression.tcl search_dir=../yosys/tests path=$SCRIPTPATH/image/bin/ yosys_exe=$SCRIPTPATH/image/bin/yosys sv2v_exe=$SCRIPTPATH/image/bin/sv2v verification
 echo "UHDM-integration Regression"
-../Surelog/tests/regression.tcl search_dir=../UHDM-integration-tests/tests path=$SCRIPTPATH/Surelog/build/bin/ verification
+../Surelog/tests/regression.tcl search_dir=../UHDM-integration-tests/tests path=$SCRIPTPATH/image/bin/ yosys_exe=$SCRIPTPATH/image/bin/yosys sv2v_exe=$SCRIPTPATH/image/bin/sv2v verification
 echo "SV2V Regression"
-../Surelog/tests/regression.tcl search_dir=../sv2v/test path=$SCRIPTPATH/Surelog/build/bin/ verification
+../Surelog/tests/regression.tcl search_dir=../sv2v/test path=$SCRIPTPATH/image/bin/ yosys_exe=$SCRIPTPATH/image/bin/yosys sv2v_exe=$SCRIPTPATH/image/bin/sv2v verification
