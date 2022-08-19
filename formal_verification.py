8c03c41705497c62d6becfb7b5c8a7197893ed69
Rafal Kolucki <rkolucki@antmicro.com>
2022-08-19 18:19:38 +0200

    Formal verification: GNU time output parsing, fixed equiv check parsing
    Signed-off-by: Rafal Kolucki <rkolucki@antmicro.com>

--- formal_verification.py
+++ formal_verification.py
@@ -4,6 +4,7 @@ import re
 import os
 import sys
 import shutil
+import datetime
 import subprocess
 
 
@@ -46,6 +47,30 @@ def get_test_top_module(test_path):
     for line in log:
         if line.startswith("Top module:"):
             return line.split("\\")[-1]
+
+
+def count_messages(test_path):
+    pass
+
+
+def get_time_result(stderr_str):
+    """
+    Extracts resource usage from GNU time output
+    """
+
+    m1 = re.search(
+        "([0-9\.:]+)user [0-9\.:]+system ([0-9]+):([0-9\.]+)elapsed ([0-9]+)%CPU",
+        stderr_str,
+    ).groups()
+    m2 = re.search("([0-9]+)maxresident", stderr_str).groups()
+
+    result = {
+        "user": float(m1[0]),
+        "elapsed": (int(m1[1]) * 60) + float(m1[2]),
+        "cpu": int(m1[3]),
+        "resident": int(m2[0]) / 1024,
+    }
+    return result
 
 
 def preprocess_sv2v(test_path, output_dir):
@@ -188,24 +213,59 @@ def get_equiv_result(output_dir):
     Parses equivalence check log to find a final result
     """
 
-    patterns = {
-        "Equivalence successfully proven": "PROVEN",
-        "Unproven": "UNPROVEN",
-        "[0-9]+ unproven": "UNPROVEN",
-        "ERROR: Can't find gold module surelog": "INVALID_MODEL_SURELOG",
-        "ERROR: Can't find gate module yosys": "INVALID_MODEL_YOSYS",
-        "Proved 0 previously unproven \$equiv cells\.": "EMPTY_MODEL",
-        "ERROR:": "ERROR_MODEL",
+    equiv_patterns = {
+        "Equivalence successfully proven": "PASS",  # EQUIVALENT
+        "Unproven": "DIFF",  # NOT_EQUIVALENT
+        "[0-9]+ unproven": "DIFF",  # NOT_EQUIVALENT
+        "ERROR: Can't find gold module surelog": "S GATE",  # INVALID_MODEL_SURELOG
+        "ERROR: Can't find gate module yosys": "Y GATE",  # INVALID_MODEL_YOSYS
+        "Proved 0 previously unproven \$equiv cells\.": "MODEL EM",  # EMPTY_MODEL
+        "ERROR:": "MODEL_ER",  # ERROR_MODEL
     }
-    log = ""
+    surelog_patterns = {
+        "Nb undefined modules: [1-9][0-9]*": "INCOM",
+        "Dumping module ": "S GATE",
+    }
 
+    log = ""
+    status = "INCONCLUSIVE"
+
+    # Check equivalence check output
     with open(os.path.join(output_dir, "equiv.out"), "r") as equiv_out:
         log = equiv_out.read()
         equiv_out.close()
 
-    for pattern in patterns.keys():
+    for pattern in equiv_patterns.keys():
         if re.search(pattern, log):
-            return patterns[pattern]
+            status = equiv_patterns[pattern]
+
+    # Inconclusive, check Surelog output
+    with open(os.path.join(output_dir, "surelog.out"), "r") as surelog_out:
+        log = surelog_out.read()
+        surelog_out.close()
+
+    for pattern in surelog_patterns.keys():
+        if re.search(pattern, log):
+            status = surelog_patterns[pattern]
+
+    for line in log.splitlines():
+        if re.search("^ERROR: ", line):
+            status = "UH PLUG"
+
+    # Inconclusive, check Yosys output
+    with open(os.path.join(output_dir, "yosys.out"), "r") as yosys_out:
+        log = yosys_out.read()
+        yosys_out.close()
+
+    if re.search("Dumping module ", log):
+        if status == "S GATE":
+            status = "NO GATE"
+        elif status == "UH PLUG":
+            status = "UH YGATE"
+        else:
+            status = "Y GATE"
+
+    return status
 
 
 def main():
@@ -229,21 +289,17 @@ def main():
 
     # Run synthesis of test's source files and export Verilog
     ret_yosys_sv2v = run_yosys(preprocessed_path, work_dir)
-    if sv2v_pass == True:
+    if sv2v_pass == False:
         ret_surelog = run_surelog(preprocessed_path, work_dir)
     else:
         ret_surelog = run_surelog(test_files_str, work_dir)
 
     # Equivalance check
-    result = "INCONCLUSIVE"
     if ret_yosys_sv2v == True and ret_surelog == True:
         top_module_name = get_test_top_module(work_dir)
         ret_equiv = run_equiv(top_module_name, work_dir)
-        print(ret_equiv.stderr)
-        if ret_equiv.returncode == 0:
-            result = get_equiv_result(work_dir)
-        else:
-            result = "FAIL"
+        print(get_time_result(ret_equiv.stderr))
+        result = get_equiv_result(work_dir)
 
     print("%s: %s" % (test_target, result))
 
