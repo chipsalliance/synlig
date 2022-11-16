226d4ae208e9e61e6eb178ec7c63bfbe8a317bdb
Robert Szczepanski <rszczepanski@antmicro.com>
2022-11-16 15:11:43 +0100

    CI: Generate separate summary for grouped tests

---
+++ .github/scripts/generate_summary.sh
@@ -0,0 +1,96 @@
+ROOT_DIR=$(dirname $(dirname $(dirname "$0")))
+TEST_RESULTS_PREFIX=$ROOT_DIR/test-results
+UHDM_PASSED_FILE=$TEST_RESULTS_PREFIX-uhdm.passed
+UHDM_FAILED_FILE=$TEST_RESULTS_PREFIX-uhdm.failed
+SYSTEMVERILOG_PASSED_FILE=$TEST_RESULTS_PREFIX-systemverilog.passed
+SYSTEMVERILOG_FAILED_FILE=$TEST_RESULTS_PREFIX-systemverilog.failed
+VCDDIFF_PASSED_FILE=$TEST_RESULTS_PREFIX-vcddiff.passed
+VCDDIFF_FAILED_FILE=$TEST_RESULTS_PREFIX-vcddiff.failed
+
+# Read passed tests if any exist
+if [[ -e $UHDM_PASSED_FILE ]]; then
+    UHDM_PASSED_NUMBER=$(wc -l $UHDM_PASSED_FILE)
+else
+    UHDM_FAILED_NUMBER=0
+fi
+if [[ -e $SYSTEMVERILOG_PASSED_FILE ]]; then
+    SYSTEMVERILOG_PASSED_NUMBER=$(wc -l $SYSTEMVERILOG_PASSED_FILE)
+else
+    SYSTEMVERILOG_PASSED_NUMBER=0
+fi
+if [[ -e $VCDDIFF_PASSED_FILE ]]; then
+    VCDDIFF_PASSED_NUMBER=$(wc -l $VCDDIFF_PASSED_FILE)
+else
+    VCDDIFF_PASSED_NUMBER=0
+fi
+
+# Read failed tests if any exist
+if [[ -e $UHDM_FAILED_FILE ]]; then
+    UHDM_FAILED_NUMBER=$(wc -l $UHDM_FAILED_FILE)
+else
+    UHDM_FAILED_NUMBER=0
+fi
+if [[ -e $SYSTEMVERILOG_FAILED_FILE ]]; then
+    SYSTEMVERILOG_FAILED_NUMBER=$(wc -l $SYSTEMVERILOG_FAILED_FILE)
+else
+    SYSTEMVERILOG_FAILED_NUMBER=0
+fi
+if [[ -e $VCDDIFF_FAILED_FILE ]]; then
+    VCDDIFF_FAILED_NUMBER=$(wc -l $VCDDIFF_FAILED_FILE)
+else
+    VCDDIFF_FAILED_NUMBER=0
+fi
+
+TOTAL_FAILED_TESTS=$(($UHDM_FAILED_NUMBER+$SYSTEMVERILOG_FAILED_NUMBER+$VCDDIFF_FAILED_NUMBER))
+TOTAL_PASSED_TESTS=$(($UHDM_PASSED_NUMBER+$SYSTEMVERILOG_PASSED_NUMBER+$VCDDIFF_PASSED_NUMBER))
+TOTAL_TESTS=$(($TOTAL_FAILED_TESTS+$TOTAL_PASSED_TESTS))
+
+if [[ ! -e $UHDM_FAILED_FILE && ! -e $SYSTEMVERILOG_FAILED_FILE && ! -e $VCDDIFF_FAILED_FILE ]]; then
+    echo ":heavy_check_mark: ALL TESTS PASSED :heavy_check_mark:" > $GITHUB_STEP_SUMMARY
+else
+    echo ":x: SOME TESTS FAILED :x:" > $GITHUB_STEP_SUMMARY
+fi
+echo "" >> $GITHUB_STEP_SUMMARY
+echo "| Name | Tests failed |" >> $GITHUB_STEP_SUMMARY
+echo "|------|--------------|" >> $GITHUB_STEP_SUMMARY
+echo "| tests-read-uhdm          | $UHDM_FAILED_NUMBER |" >> $GITHUB_STEP_SUMMARY
+echo "| tests-read-systemverilog | $SYSTEMVERILOG_FAILED_NUMBER |" >> $GITHUB_STEP_SUMMARY
+echo "| tests-read-vcddiff       | $VCDDIFF_FAILED_NUMBER |" >> $GITHUB_STEP_SUMMARY
+echo "| **Total** | **$TOTAL_FAILED_TESTS** |" >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY
+echo "$TOTAL_FAILED_TESTS out of total $TOTAL_TESTS failed." >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY
+
+echo "# Results for tests-read-uhdm:" >> $GITHUB_STEP_SUMMARY
+echo "| Name | Result |" >> $GITHUB_STEP_SUMMARY
+echo "|------|--------|" >> $GITHUB_STEP_SUMMARY
+while IFS= read -r LINE; do
+    echo "$LINE" >> $GITHUB_STEP_SUMMARY
+done < $UHDM_FAILED_FILE
+while IFS= read -r LINE; do
+    echo "$LINE" >> $GITHUB_STEP_SUMMARY
+done < $UHDM_PASSED_FILE
+echo "" >> $GITHUB_STEP_SUMMARY
+
+echo "# Results for tests-read-systemverilog:" >> $GITHUB_STEP_SUMMARY
+echo "| Name | Result |" >> $GITHUB_STEP_SUMMARY
+echo "|------|--------|" >> $GITHUB_STEP_SUMMARY
+while IFS= read -r LINE; do
+    echo "$LINE" >> $GITHUB_STEP_SUMMARY
+done < $SYSTEMVERILOG_FAILED_FILE
+while IFS= read -r LINE; do
+    echo "$LINE" >> $GITHUB_STEP_SUMMARY
+done < $SYSTEMVERILOG_PASSED_FILE
+echo "" >> $GITHUB_STEP_SUMMARY
+
+echo "# Results for tests-read-vcddiff:" >> $GITHUB_STEP_SUMMARY
+echo "| Name | Result |" >> $GITHUB_STEP_SUMMARY
+echo "|------|--------|" >> $GITHUB_STEP_SUMMARY
+while IFS= read -r LINE; do
+    echo "$LINE" >> $GITHUB_STEP_SUMMARY
+done < $VCDDIFF_FAILED_FILE
+while IFS= read -r LINE; do
+    echo "$LINE" >> $GITHUB_STEP_SUMMARY
+done < $VCDDIFF_PASSED_FILE
+echo "" >> $GITHUB_STEP_SUMMARY
--- .github/scripts/run_group_test.sh
+++ .github/scripts/run_group_test.sh
@@ -1,23 +1,23 @@
-echo "|Name | Result|" > $GITHUB_STEP_SUMMARY
-echo "|-----|-------|" >> $GITHUB_STEP_SUMMARY
-
 ROOT_DIR=$(dirname $(dirname $(dirname "$0")))
 TEST_CASES="$(cd UHDM-integration-tests && python list.py -d tests -s ibex swerv synthesis opentitan serv serv-minimal hello-uvm assignment-pattern Forever BitsCallOnType OneClass Continue AnonymousUnion FunctionOnDesignLevel ParameterUnpackedArray VoidFunction2Returns PatternStruct ImportedFunctionCallInModuleAndSubmodule VoidFunctionWithoutReturn cmake PutC OneThis CastInFunctionInGenBlock PatternType FunctionOutputArgument GetC ForkJoinTypes EnumFirstInInitial ImportFunction DpiChandle Disable EnumFirst TypedefOnFileLevel UnsizedConstantsParameterParsing Fork PatternInFunction TypedefVariableDimensions ParameterUnpackedLogicArray SelectFromUnpackedInFunction PatternReplication VoidFunction MultiplePrints BitSelectPartSelectInFunction ImportPackageWithFunction ParameterPackedArray StringAssignment SystemFunctions ParameterDoubleUnderscoreInSvFrontend OutputSizeWithParameterOfInstanceInitializedByStructMember ParameterOfSizeOfParametrizedPort ParameterOfSizeOfParametrizedPortInSubmodule ParameterOfSizeOfPort StringAssignConcatenation StringLocalParamInitByConcatenation StringWithBackslash FunctionWithOverriddenParameter RealValue BitsCallOnParametetrizedTypeFromPackage AssignToUnpackedUnionFieldAndReadOtherField IndexedPartSelectInFor NestedPatternPassedAsPort NestedStructArrayParameterInitializedByPatternPassedAsPort PartSelectInFor SelfSelectsInBitSelectAfterBitSelect StructArrayParameterInitializedByPatternPassedAsPort SelectGivenBySelectOnParameterInFunction StreamOperatorBitReverseFunction)"
 TEST_CASES=$(echo $TEST_CASES | sed "s/[][,\"]//g") # Remove characters '[', ']', '"' and ',' from json like array
 
+mkdir -p $ROOT_DIR/test-results
 for TEST_CASE in $TEST_CASES;
 do
     export TEST_CASE
     $ROOT_DIR/UHDM-integration-tests/.github/ci.sh
+    TEST_CASE=$(echo $TEST_CASE | sed "s/tests\///g")
     if [ $? -eq 0 ]; then
-        echo "|$TEST_CASE | :heavy_check_mark: **PASS**|" >> $GITHUB_STEP_SUMMARY
+        echo "|$TEST_CASE | :heavy_check_mark: **PASS**|" >> $ROOT_DIR/test-results/test-results.passed
     else
-        echo "|$TEST_CASE | :x: **FAIL**|" >> $GITHUB_STEP_SUMMARY
-        export FAILED=1
+        echo "|$TEST_CASE | :x: **FAIL**|" >> $ROOT_DIR/test-results/test-results.failed
+        RET=1
     fi
 done
 
-# Leave with error status if any test failed
-if [[ $FAILED -ne 0 ]]; then
+
+# Leave with non-zero error status if any test failed
+if [[ $RET -ne 0 ]]; then
     exit 1
 fi
