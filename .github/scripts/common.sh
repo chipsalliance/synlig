be7f8da5cd96baa7892e7e017a3d9b1decf22015
Kamil Rakoczy <krakoczy@antmicro.com>
2022-11-18 16:16:22 +0100

    Grep for vivado version
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -8,6 +8,7 @@ function enable_vivado() {
     ls /opt/Xilinx/Vivado
     source /opt/Xilinx/Vivado/$1/settings64.sh
     # close stdin in case vivado -version fails
-    vivado -version <&-
-    echo "Vivado exit code: $?"
+    vivado -version <&- | grep $1
+    ec = $?
+    echo "Vivado exit code: $ec"
 }
