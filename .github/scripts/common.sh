5daea48eece00bfaa75367627ec957ccc96442dc
Kamil Rakoczy <krakoczy@antmicro.com>
2022-11-18 15:19:02 +0100

    Close stdin
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -7,5 +7,6 @@ function enable_vivado() {
     ln -s /mnt/aux/Xilinx /opt/Xilinx
     ls /opt/Xilinx/Vivado
     source /opt/Xilinx/Vivado/$1/settings64.sh
-    vivado -version
+    # close stdin in case vivado -version fails
+    vivado -version <&-
 }
