75bce11188ca5ee13a2a68fc66b2957d9bc2279d
Magdalena Andrys <mandrys@antmicro.com>
2022-12-08 11:17:58 +0100

    Remove depth limit and test all subdirectories in the test suites
    Signed-off-by: Magdalena Andrys <mandrys@antmicro.com>

--- run_fv_tests.sh
+++ run_fv_tests.sh
@@ -4,4 +4,4 @@ testsuite=$(realpath $1)
 pushd $testsuite
 ../../Surelog/tests/create_batch_script.tcl  ext=.v,.sv batch=batch.txt options="+incdir+. -parse -nocache -nobuiltin -nonote -noinfo -timescale=1ns/1ns" verification
 popd
-find $testsuite -maxdepth 1 -mindepth 1 -type d -exec python3 ./formal_verification.py {} \;
+find $testsuite -mindepth 1 -type d -exec python3 ./formal_verification.py {} \;
