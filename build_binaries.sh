04af07f3fc12a393dfbd11dc7357eec5941b05de
Tomasz Gorochowik <tgorochowik@antmicro.com>
2023-01-18 10:34:20 +0100

    Rename the plugins submodule path
    Symbiflow->f4pga

--- build_binaries.sh
+++ build_binaries.sh
@@ -45,7 +45,7 @@ if [ "$PLUGIN_ASAN" -eq "1" ]; then
   PLUGIN_LDFLAGS="-fsanitize=address -fcheck-new -fno-omit-frame-pointer -static-libasan"
 fi
 export PATH=$INSTALL_PATH/bin:${PATH}
-UHDM_INSTALL_DIR=$INSTALL_PATH LDFLAGS=$PLUGIN_LDFLAGS make -C $PWD/yosys-symbiflow-plugins/ install -j$(nproc)
+UHDM_INSTALL_DIR=$INSTALL_PATH LDFLAGS=$PLUGIN_LDFLAGS make -C $PWD/yosys-f4pga-plugins/ install -j$(nproc)
 #sv2v
 if [ "$BUILD_SV2V" -eq "1" ]; then
 wget -qO- https://get.haskellstack.org/ | sh -s - -f -d $INSTALL_PATH/bin
