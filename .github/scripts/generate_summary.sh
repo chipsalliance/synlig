44bb116391bf5217d689351439884c85b32ca1a9
Mariusz Glebocki <mglebocki@antmicro.com>
2023-02-23 09:46:50 +0100

    CI: Reorganize parsing tests result summary.

--- .github/scripts/generate_summary.sh
+++ .github/scripts/generate_summary.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e -u -o pipefail
+shopt -s nullglob
+shopt -s extglob
+
 ROOT_DIR=$(dirname $(dirname $(dirname "$0")))
 TEST_RESULTS_PREFIX=$ROOT_DIR/test-results
 UHDM_PASSED_FILE=$TEST_RESULTS_PREFIX-uhdm.passed
@@ -39,34 +44,80 @@ else
     echo ":x: SOME TESTS FAILED :x:" > $GITHUB_STEP_SUMMARY
 fi
 echo "" >> $GITHUB_STEP_SUMMARY
-echo "| Name | Tests failed | Tests passed |" >> $GITHUB_STEP_SUMMARY
-echo "|------|--------------|--------------|" >> $GITHUB_STEP_SUMMARY
-echo "| tests-read-uhdm          | $UHDM_FAILED_NUMBER | $UHDM_PASSED_NUMBER |" >> $GITHUB_STEP_SUMMARY
-echo "| tests-read-systemverilog | $SYSTEMVERILOG_FAILED_NUMBER | $SYSTEMVERILOG_PASSED_NUMBER |" >> $GITHUB_STEP_SUMMARY
-echo "| **Total** | **$TOTAL_FAILED_TESTS** | **$TOTAL_PASSED_TESTS** |" >> $GITHUB_STEP_SUMMARY
+echo "| Tool/Parser          | Failed | Passed |" >> $GITHUB_STEP_SUMMARY
+echo "|:---------------------|-------:|-------:|" >> $GITHUB_STEP_SUMMARY
+echo "| Surelog              | $UHDM_FAILED_NUMBER | $UHDM_PASSED_NUMBER |" >> $GITHUB_STEP_SUMMARY
+echo "| SystemVerilog Plugin | $SYSTEMVERILOG_FAILED_NUMBER | $SYSTEMVERILOG_PASSED_NUMBER |" >> $GITHUB_STEP_SUMMARY
+echo "| **Total**            | **$TOTAL_FAILED_TESTS** | **$TOTAL_PASSED_TESTS** |" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
 echo "$TOTAL_FAILED_TESTS out of total $TOTAL_TESTS failed." >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
 
-echo "# Results for tests-read-uhdm:" >> $GITHUB_STEP_SUMMARY
-echo "| Name | Result |" >> $GITHUB_STEP_SUMMARY
-echo "|------|--------|" >> $GITHUB_STEP_SUMMARY
-while IFS= read -r LINE; do
-    echo "$LINE" >> $GITHUB_STEP_SUMMARY
-done < $UHDM_FAILED_FILE
-while IFS= read -r LINE; do
-    echo "$LINE" >> $GITHUB_STEP_SUMMARY
-done < $UHDM_PASSED_FILE
-echo "" >> $GITHUB_STEP_SUMMARY
+declare -A surelog_results=()
+declare -A sv_plugin_results=()
+for result_file in "$SYSTEMVERILOG_FAILED_FILE" "$SYSTEMVERILOG_PASSED_FILE"; do
+    if [[ -f "$result_file" ]]; then
+        while read -r test_name test_result; do
+            sv_plugin_results[$test_name]=${test_result}
+        done < "$result_file"
+    fi
+done
+for result_file in "$UHDM_FAILED_FILE" "$UHDM_PASSED_FILE"; do
+    if [[ -f "$result_file" ]]; then
+        while read -r test_name test_result; do
+            surelog_results[$test_name]=${test_result}
+        done < "$result_file"
+    fi
+done
 
-echo "# Results for tests-read-systemverilog:" >> $GITHUB_STEP_SUMMARY
-echo "| Name | Result |" >> $GITHUB_STEP_SUMMARY
-echo "|------|--------|" >> $GITHUB_STEP_SUMMARY
-while IFS= read -r LINE; do
-    echo "$LINE" >> $GITHUB_STEP_SUMMARY
-done < $SYSTEMVERILOG_FAILED_FILE
-while IFS= read -r LINE; do
-    echo "$LINE" >> $GITHUB_STEP_SUMMARY
-done < $SYSTEMVERILOG_PASSED_FILE
-echo "" >> $GITHUB_STEP_SUMMARY
+declare -a results_00=()
+declare -a results_10=()
+declare -a results_01=()
+declare -a results_11=()
+
+declare -r pass_str=':heavy_check_mark: **PASS**'
+declare -r fail_str=':x: **FAIL**'
+
+for test_name in "${!surelog_results[@]}"; do
+    case "${surelog_results[$test_name]:-0}${sv_plugin_results[$test_name]:-0}" in
+        [^1][^1])
+            results_00+=("| $fail_str | $fail_str | $test_name |")
+        ;;
+        1[^1])
+            results_10+=("| $pass_str | $fail_str | $test_name |")
+        ;;
+        [^1]1)
+            results_01+=("| $fail_str | $pass_str | $test_name |")
+        ;;
+        11)
+            results_11+=("| $pass_str | $pass_str | $test_name |")
+        ;;
+    esac
+done
+
+print_ln() { printf '%s\n' "$@"; }
+
+{
+    if (( ${#results_00[@]} + ${#results_10[@]} + ${#results_01[@]} > 0 )); then
+        print_ln '<details open>'
+        print_ln '<summary><strong>List of failed tests</strong></summary>'
+        print_ln ''
+        print_ln '| Surelog | SV Plugin | Test |'
+        print_ln '|:-------:|:---------:|:-----|'
+        print_ln "${results_00[@]}" | sort
+        print_ln "${results_10[@]}" | sort
+        print_ln "${results_01[@]}" | sort
+        print_ln ''
+        print_ln '</details>'
+    fi
+
+    print_ln '<details>'
+    print_ln '<summary><strong>List of passed tests</strong></summary>'
+    print_ln ''
+    print_ln '| Surelog | SV Plugin | Test |'
+    print_ln '|:-------:|:---------:|:-----|'
+    print_ln "${results_11[@]}" | sort
+    print_ln ''
+    print_ln '</details>'
+} >> $GITHUB_STEP_SUMMARY
