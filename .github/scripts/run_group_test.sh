5234a668d39fcdb9d579418f0dabf30db6e4bb7b
Kamil Rakoczy <krakoczy@antmicro.com>
2023-03-08 10:16:15 +0100

    Automatically update logs when they changed
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- .github/scripts/run_group_test.sh
+++ .github/scripts/run_group_test.sh
@@ -7,11 +7,17 @@ else
 fi
 TEST_CASES=$(echo $TEST_CASES | sed "s/[][,\"]//g") # Remove characters '[', ']', '"' and ',' from json like array
 
+LOG_DIR=$ROOT_DIR/logs
+
+mkdir -p $LOG_DIR
+
 mkdir -p $ROOT_DIR/test-results
 for TEST_CASE in $TEST_CASES;
 do
     export TEST_CASE
-    $ROOT_DIR/UHDM-integration-tests/.github/ci.sh
+    mkdir -p $LOG_DIR/$TEST_CASE
+    ROOT_DIR_ABSOLUTE=$(realpath $ROOT_DIR)/
+    $ROOT_DIR/UHDM-integration-tests/.github/ci.sh | sed -e 's#'${ROOT_DIR_ABSOLUTE}'##g' | sed -e 's/\(End of script\).*//' | sed -e 's/\(Time spent\).*//' | tee $LOG_DIR/$TEST_CASE/ast.log
     TEST_RET=$?
     TEST_CASE="${TEST_CASE//tests\//}"
     if [[ $TEST_RET -eq 0 ]]; then
