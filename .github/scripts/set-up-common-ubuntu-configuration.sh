39e86a3849d4ddb3eb5dfdd26216e879cc9ea63a
Mariusz Glebocki <mglebocki@antmicro.com>
2023-03-27 11:53:39 +0200

    CI: Create .gitconfig in job init script.

--- .github/scripts/set-up-common-ubuntu-configuration.sh
+++ .github/scripts/set-up-common-ubuntu-configuration.sh
@@ -81,3 +81,23 @@ printf '%s\n' \
 end_group
 
 #-----------------------------------------------------------------------------
+begin_group 'Configure git'
+#-----------------------------------------------------------------------------
+
+# Use "some reasonable default" parallel fetch operations.
+# https://git-scm.com/docs/git-config#Documentation/git-config.txt-fetchparallel
+cat >> ~/.gitconfig <<-EOF
+[fetch]
+	parallel=0
+EOF
+# Github dropped support for unauthorized git:
+# https://github.blog/2021-09-01-improving-git-protocol-security-github/
+# Make sure we always use https:// instead of git://
+cat >> ~/.gitconfig <<-EOF
+[url "https://github.com/"]
+	insteadOf=git://github.com/
+EOF
+
+end_group
+
+#-----------------------------------------------------------------------------
