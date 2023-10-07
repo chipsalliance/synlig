06d32618bea9a697c6c7de656b3d2a4a1bd454b0
Alain Dargelas <alain.dargelas@gmail.com>
2023-10-06 22:29:56 -0700

    stop using allModules and support for packed_array

--- tests/simple_tests/PackedArrayPort/top.sv
+++ tests/simple_tests/PackedArrayPort/top.sv
@@ -1,12 +1,12 @@
-package pack;
-  typedef enum logic { FOO } foo;
+package pack_pkg;
+  typedef enum logic { fOO } foo;
 endpackage
 
-module dut(input pack::foo [1:0] inp[2], output pack::foo [1:0] out[2]);
+module dut(input pack_pkg::foo [1:0] inp[2], output pack_pkg::foo [1:0] out[2]);
   assign out = inp;
 endmodule
 
-module top(input pack::foo [1:0] inp[2], output pack::foo [1:0] out[2]);
+module top(input pack_pkg::foo [1:0] inp[2], output pack_pkg::foo [1:0] out[2]);
   dut u_dut (.inp(inp), .out(out));
 endmodule
 
