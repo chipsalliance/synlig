01111c512ced3faaeae12a875e7a21f115580c63
alaindargelas <63669492+alaindargelas@users.noreply.github.com>
2023-10-26 21:28:51 -0700

    Fix bug with typespecs declared in package (#2097)
    Co-authored-by: Krzysztof Bieganski <kbieganski@antmicro.comi>

---
+++ tests/simple_tests/PackageLogicTypespec/dut.sv
@@ -0,0 +1,9 @@
+package my_pkg;
+
+  typedef logic [31:0] my_type_t;
+
+endpackage
+
+module dut (input wire clk, input my_pkg::my_type_t my_in, output logic [31:0] my_out);
+  assign my_out = my_in;
+endmodule
