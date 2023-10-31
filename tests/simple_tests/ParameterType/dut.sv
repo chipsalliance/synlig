c5dc65bb1254682ca8f0586aa09c445a7e0254c5
Alain Dargelas <alain.dargelas@gmail.com>
2023-10-30 21:24:38 -0700

    test update

--- tests/simple_tests/ParameterType/dut.sv
+++ tests/simple_tests/ParameterType/dut.sv
@@ -1,6 +1,6 @@
+module dut(input logic clk, input logic i, output logic o);
+ localparam int GW_CONFIG2[2:0] = '{default:0} ;
 
-module dut(input logic clk);
-
-localparam int GW_CONFIG2[2:0] = '{default:0} ;
+ assign o = i;
 
 endmodule
