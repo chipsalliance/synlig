d5d47de74e7cc81514ae2b481fbcbff20103fee5
Alain Dargelas <alain.dargelas@gmail.com>
2023-11-01 08:48:53 -0700

    test udpate

--- tests/simple_tests/BlackBox/yosys_script.tcl
+++ tests/simple_tests/BlackBox/yosys_script.tcl
@@ -3,4 +3,6 @@ source ../yosys_common.tcl
 prep -top \\dut
 write_verilog
 write_verilog yosys.sv
-sim -clock clk -rstlen 10 -vcd dump.vcd
+# The sim command does not work for designs with blackbox,
+# it does not allow to add the blackboxes actual models back into the simulation 
+# sim -clock clk -rstlen 10 -vcd dump.vcd
