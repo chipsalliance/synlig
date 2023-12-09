4b3a2236a5ec4662f040955f24e2c5066ac9bef2
Alain Dargelas <alain.dargelas@gmail.com>
2023-12-08 20:43:40 -0800

    Simplify hier path

---
+++ tests/simple_tests/SimplifyHierPath/dut.sv
@@ -0,0 +1,11 @@
+module module_scope_Example(o1);
+   parameter [31:0] v1 = 10;
+   output wire [31:0] o1;
+   assign module_scope_Example.o1 = module_scope_Example.v1;
+endmodule
+
+module dut(
+   output wire [31:0] a1
+);
+   module_scope_Example #(.v1(11)) a  (a1);
+endmodule
