912f1b74da338f9cb74fcd6df25a4d95ee071d7e
mszelwiga <mszelwiga@internships.antmicro.com>
2024-08-01 11:45:03 +0200

    Unify yosys_mod function names
    This commit also removes unnecessary calls to yosys functions

--- third_party/yosys_mod/synlig_simplify.h
+++ third_party/yosys_mod/synlig_simplify.h
@@ -3,6 +3,7 @@
 namespace systemverilog_plugin
 {
 using ys_size_type = int; // Makes it easy to change if changed upstream.
-bool simplify(Yosys::AST::AstNode *ast_node, bool const_fold, bool at_zero, bool in_lvalue, int stage, int width_hint, bool sign_hint, bool in_param);
+bool synlig_simplify(Yosys::AST::AstNode *ast_node, bool const_fold, bool at_zero, bool in_lvalue, int stage, int width_hint, bool sign_hint,
+                     bool in_param);
 void synlig_expand_genblock(Yosys::AST::AstNode *current_node, std::string prefix, bool only_resolve_scope);
 } // namespace systemverilog_plugin
